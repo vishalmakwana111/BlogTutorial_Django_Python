3d42ba2b97611856da61ba7243c5045bebc31dbf
vishal makwana <makwanavishal2511@gmail.com>
2023-06-25 17:31:29 +0530

    BlogTutorialDjango Created Sucessfully

---
+++ blogtutorial/users/views.py
@@ -0,0 +1,34 @@
+# from django.shortcuts import render, redirect
+# from django.views import View
+# from .forms import UserRegisterForm
+
+# class RegisterView(View):
+# 	def get(self, request):
+# 		form = UserRegisterForm()
+# 		return render(request, 'users/register.html', {'form': form})
+
+# 	def post(self, request):
+# 		form = UserRegisterForm(request.POST)
+
+# 		if form.is_valid():
+# 			form.save()
+# 			return redirect('index')
+
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegisterForm
+
+class RegisterView(View):
+	def get(self, request):
+		form = UserRegisterForm()
+		return render(request, 'users/register.html', {'form': form})
+
+	def post(self, request):
+		form = UserRegisterForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			return redirect('index')
+
+		# If the form is not valid, re-render the register template with the form
+		return render(request, 'users/register.html', {'form': form})
